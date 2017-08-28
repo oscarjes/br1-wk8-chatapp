ecbb8f2957f5bbc2caa84e84d7dbcb4c6f44e130
oscarjes <oscar@jesionek.co>
2017-08-28 14:56:14 +0700

    chatroom broadcast

---
+++ app/assets/javascripts/channels/chat.js
@@ -0,0 +1,18 @@
+function setupRoom(roomId) {
+  // first argument can be a string or object
+  // here we use an object to specify the extra params
+  App.chat = App.cable.subscriptions.create(
+    {channel: "ChatChannel", id: roomId},
+    {
+      connected: function(){
+        console.log('#ChatChannel connected', this);
+      },
+      disconnceted: function() {
+        console.log('#ChatChannel disconnected');
+      },
+      received: function(data) {
+        console.log('#ChatChannel received:', data);
+      }
+    }
+  );
+}
